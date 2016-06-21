82d3fca8ad967b0279c7b445e8677fb40eda1d72
krisrandall <kris.randall@gmail.com>
2016-06-21 11:25:23 +1000

    add mini blockies icon in list view

--- app/pages/wallet-home/wallet-home.ts
+++ app/pages/wallet-home/wallet-home.ts
@@ -5,6 +5,7 @@ import {WalletDetail} from '../wallet-detail/wallet-detail';
 import {WalletImportPage} from '../wallet-import/wallet-import';
 import {Data} from '../../providers/data/data';
 
+declare var blockies: any;
 
 @Page({
   templateUrl: 'build/pages/wallet-home/wallet-home.html',
@@ -16,22 +17,33 @@ export class WalletHome {
 
   constructor(private nav: NavController, private dataService: Data) {
 
-    this.dataService.getData().then((todos) => {
+    this.dataService.getData().then((walletList) => {
 
-      if (todos){
-        this.items = JSON.parse(todos); 
+      if (walletList){
+        this.items = JSON.parse(walletList);
+
+        // create icons
+        for (var i = 0; i < this.items.length; i++ ) {
+          this.items[i].miniIcon = this.getItemIcon(this.items[i]);
+        }
       }
 
     });
 
+
+  }
+
+  getItemIcon(item) {
+    return blockies.create({ 'seed': item.address, size: 6, scale: 1 }).toDataURL();
   }
 
 
-  addItem(){
+  addItem() {
     this.nav.push(WalletCreate, { walletHome: this } ); // it calls WalletHome.saveItem
   }
 
   saveItem(item){
+    item.miniIcon = this.getItemIcon(item);
     this.items.push(item);
     this.dataService.save(this.items);
   }
