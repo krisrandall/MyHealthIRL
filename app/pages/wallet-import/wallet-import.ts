3a02272d9e64d31d800de4c6f918b0416208ce0e
krisrandall <kris.randall@gmail.com>
2016-06-24 10:39:36 +1000

    WIP commit - this version reaches inside the authenticated window .. possibly the token could be grabbed and used ... but this code is also getting too damn ugly. i'm going to go with a different path. committing these changes to save this state - but abandoning this branch for now

--- app/pages/wallet-import/wallet-import.ts
+++ app/pages/wallet-import/wallet-import.ts
@@ -1,62 +1,100 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+
+declare var Global: any;
+
+
 @Component({
-	templateUrl: 'build/pages/wallet-import/wallet-import.html',
+	templateUrl: 'build/pages/wallet-import/wallet-import.html'
 })
 export class WalletImportPage {
+
+	private iframeContent;
+	private defaultIFrameContent = "NOT_YET_CODED"; /* this is shown if offline OR if they close the kloudless dialog */
+	private fileAccessServerChoose = "http://cocreations.com.au/sicoor/api/kloudless.choose.wrapper.html";
+	private timeHasPassed = false;
+
+
+
 	constructor(public nav: NavController) {
 
-		$(document).ready(function() {
 
-			var explorer = window.Kloudless.explorer({
-				app_id: 'XiW25w8Rchrozdp7JM2O4socjr_kDsaErEZGuvcpZDBu5me9',
-				multiselect: true,
-				computer: true,
-				services: [
-					// see the services var in https://github.com/Kloudless/file-explorer/blob/master/src/explorer/js/config.js 
-					// These 4 are popular and fit tidly on the screen :
-					'dropbox',
-					'gdrive',
-					//'box',
-					'evernote',
-					//'skydrive',
-					//'sugarsync',
-					//'sharefile',
-					//'egnyte',
-					'sharepoint',
-					//'onedrivebiz',
-					//'hubspot',
-					//'salesforce',
-					//'smb',
-					//'cmis',
-					//'alfresco',
-					//'alfresco_cloud',
-					//'jive',
-					//'webdav',
-					//'cq5',
-				]
-			});
+		// This function handles the response from the kloudless file chooser
+		var global = {
+			HandlePopupResult : {
+				Choosen : function(results) {
 
-			explorer.on('success', function(files) {
-				alert('My files :' + JSON.stringify(files, null, 2));
-			});
+					alert('here are the results!');
+					alert(JSON.stringify(results));
+				}
+			}
+		}
+		window.Global = global;
 
-			explorer.choosify(document.getElementById('chooser'));
 
-			var files = [{
-				url: "https://s3-us-west-2.amazonaws.com/static-assets.kloudless.com/static/logo_white.png",
-				name: "kloudless-logo.png"
-			}];
+		// show retry button after some time has passed
+		window.setTimeout(function() {
+			this.timeHasPassed = true;
+		}.bind(this), 6000);
 
-			explorer.savify(document.getElementById("saver"), files);
 
+		this.openFileChooser();
+	}
+
+	openFileChooser() {
+
+		// We need to determine if we are online or offline
+		this.iframeContent = this.fileAccessServerChoose + "?force_no_cache="+Math.random(); 
+
+		/*
+		Note that we don't have a proper window object here so the postMessage back from
+		kloudless cannot work - instead we need to do this hack that lets us look inside
+		the child browser for the success string 
+		*/
+		var successString : String = "You have successfully connected your account.";
+		var win = window.open(this.iframeContent, "_blank",
+			 "EnableViewPortScale=no" );
+
+		// Once the InAppBrowser finishes loading
+		win.addEventListener( "loadstop", function() {
+
+		    // Start an interval
+		    var loop = setInterval(function() {
+
+		        // Execute JavaScript to check for the existence of a name in the
+		        // child browser's localStorage.
+		        win.executeScript(
+		            {
+		                code: "((document.body.textContent || document.body.innerText).indexOf('"+successString+"') > -1)"
+		            },
+		            function( values ) {
+		                var gotSuccess = values[ 0 ];
+
+		                // If a name was set, clear the interval and close the InAppBrowser.
+		                if ( gotSuccess ) {
+		                    clearInterval( loop );
+
+							alert('AND HERE WE ARE !!!');
+							
+							console.log(document.body.innerText);
+							
+							console.log(' ---- ');
+
+							console.log(receiveMessage);
+
+/*
+							win.close();
+							win.close();
+	*/
+		                }
+		            }
+		        );
+		    }, 800);
 		});
 
+
 	}
 
-	my_func() {
-		window.open('https://api.kloudless.com/v0/oauth/services/alfresco?origin=https%3A%2F%2Fstatic-cdn.kloudless.com&modifiers=normal&referrer=explorer&state=64079998105&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=token&client_id=XiW25w8Rchrozdp7JM2O4socjr_kDsaErEZGuvcpZDBu5me9&request_id=64079998105&scope=alfresco',
-			'_blank');
-	}
+
 }
