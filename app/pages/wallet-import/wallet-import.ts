d26582c855821376dd76af6bb90b936f292a59e1
krisrandall <kris.randall@gmail.com>
2016-06-24 16:07:50 +1000

    Successfully get the dropbox auth token !

--- app/pages/wallet-import/wallet-import.ts
+++ app/pages/wallet-import/wallet-import.ts
@@ -20,7 +20,7 @@ export class WalletImportPage {
 			"dropbox": {
 				"base_url": "https://www.dropbox.com/1/oauth2/authorize?response_type=token&state=whatever",
 				"client_id": "7o1hlldcmb28dk5",
-				"redirect_uri": "https://www.dropbox.com/1/oauth2/redirect_receiver"
+				"redirect_uri": "https://take5app.co/sicoor/dropbox.redirect_back_to_app.html"
 			}
 		}
 		this.fileStorageAuthUrl = filesSource.dropbox.base_url 
@@ -30,20 +30,49 @@ export class WalletImportPage {
 
 		/* TO DO : check state here - if we have a valid token then we don't need to auth ... */
 
-
 		// need to clear cache here 'cause caching seems to be happening, use this plugin :
 		// https://github.com/moderna/cordova-plugin-cache
 		if (window.cache) {
 			window.cache.clear(function() {
-				window.location = this.fileStorageAuthUrl;
-			}.call(this));			
+
+				var win = window.open(this.fileStorageAuthUrl, "_blank", "_location=no");
+				
+				win.addEventListener('loadstop', function do_loadstop(e) {
+
+					// Start an interval
+					var loop = setInterval(function() {
+						// Execute JavaScript to check for the existence of a name in the
+						// child browser's localStorage.
+						win.executeScript(
+							{
+								code: "document.getElementById('access_token').value"
+							},
+							function(values) {
+
+								var access_token = values[0];
+
+								if (access_token) {
+
+									alert('the token is ' + access_token);
+									//alert('account id is ' + account_id);
+
+									clearInterval(loop);
+									win.close();
+
+								}
+							}
+						);
+					}, 400);
+				});
+
+				win.addEventListener('exit', function do_exit() { 
+
+				});
+
+			}.bind(this));
 		} else {
-			console.log('No window.cache pluggin -- you must be running in the browswer, or have not installed plugins !');
-			window.location = this.fileStorageAuthUrl;
+			console.log('no cache so doing nothing !! -- this just here cause im in a hurry');
 		}
-
-
-
 
 	}
 }
